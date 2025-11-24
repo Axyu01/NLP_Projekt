c034dd1bf2f43a2f0303a7f617cafef598d150a7
Julia Wysocka <268329@student.pwr.edu.pl>
2025-11-24 22:02:20 +0100

    update preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -15,3 +15,12 @@ class_percent = class_counts / class_counts.sum() * 100
 
 print("Class counts:\n", class_counts)
 print("\nClass percentage:\n", class_percent)
+
+train_df = train_df[['tweet', 'subtask_a']].dropna()
+
+label_map = {"NOT": 0, "OFF": 1}
+labels = train_df['subtask_a'].map(label_map).tolist()
+
+texts = train_df['tweet'].tolist()
+
+print(texts[0], labels[0])
